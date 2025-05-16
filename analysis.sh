d8a91d52a8d1fa768eb29c9508aaa7ef1e3d3a49
Laine Mulvay <laine.mulvay@gmail.com>
2025-05-16 13:29:51 +0800

    required correlations working and echoed correclty

--- analysis.sh
+++ analysis.sh
@@ -38,4 +38,38 @@ domain_name=$(echo "$most_popular_domain" | gawk '{$1=""; print substr($0,2)}')
 
 echo "The most popular game mechanics is $mechanics_name found in $mechanics_count games"
 echo "The most popular game domain is $domain_name found in $domain_count games"
+echo ""
 rm mechanics.tmp domains.tmp
+
+# correlation function
+correlation() {
+    # Calculate Pearson correlation between two columns
+    gawk -F'\t' -v col1="$1" -v col2="$2" '
+    NR > 1 {
+        x[NR] = $col1
+        y[NR] = $col2
+        sum_x += $col1
+        sum_y += $col2
+        n++
+    }
+    END {
+        mean_x = sum_x / n
+        mean_y = sum_y / n
+        
+        for (i = 2; i <= NR; i++) {
+            sum_xy += (x[i] - mean_x) * (y[i] - mean_y)
+            sum_xx += (x[i] - mean_x) * (x[i] - mean_x)
+            sum_yy += (y[i] - mean_y) * (y[i] - mean_y)
+        }
+        
+        r = sum_xy / sqrt(sum_xx * sum_yy)
+        printf "%.9f", r
+    }' "$filename"
+}
+
+
+# find correlation between publication year and average rating 
+echo "Testing correlation between the year of publication and the average rating is $(printf "%.3f" $(correlation 3 9))"
+
+# find correlation between game complexity and average rating 
+echo "Testing correlation between the game complexity and the average rating is $(printf "%.3f" $(correlation 11 9))"
